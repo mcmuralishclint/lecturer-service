e4716681946c2068c9540dd9ce7ec6b611806a7e
muralishc <muralish.clinton@circles.asia>
2022-06-16 16:58:01 +0530

    update create or find by method

--- controller/google.go
+++ controller/google.go
@@ -2,11 +2,13 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/config"
+	"github.com/mcmuralishclint/personal_tutor/lecturer-service/models"
 )
 
 func GoogleLogin(res http.ResponseWriter, req *http.Request) {
@@ -40,5 +42,43 @@ func GoogleCallback(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(res, "JSON parsing failed")
 	}
 
-	fmt.Fprintln(res, string(userData))
+	existing, email, err := createOrQueryUser(string(userData))
+	if err != nil {
+		fmt.Fprintln(res, "Unable to create or retreive user")
+	}
+
+	if existing {
+		fmt.Fprintln(res, "Welcome back "+email)
+	} else {
+		fmt.Fprintln(res, "Greetings "+email)
+	}
+
+}
+
+func createOrQueryUser(userData string) (bool, string, error) {
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(userData), &jsonMap)
+	email := jsonMap["email"].(string)
+	lecturer, err := models.FindLecturer(email)
+	if err != nil {
+		return false, "", err
+	}
+	if (lecturer == models.Lecturer{}) {
+		//Create User
+		lecturer := models.Lecturer{
+			Email:      email,
+			FullName:   jsonMap["name"].(string),
+			GivenName:  jsonMap["given_name"].(string),
+			FamilyName: jsonMap["family_name"].(string),
+			Verified:   jsonMap["verified_email"].(bool),
+			Picture:    jsonMap["picture"].(string),
+		}
+		_, err := models.CreateLecturer(lecturer)
+		if err != nil {
+			return false, email, err
+		}
+		return false, email, nil
+	} else {
+		return true, email, nil
+	}
 }
