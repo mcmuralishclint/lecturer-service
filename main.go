6442986c5a7085149e16920f4150e6aa50f8799b
muralishc <muralish.clinton@circles.asia>
2022-06-16 20:22:48 +0530

    add gorilla mux for routing

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/controller"
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/models"
 )
 
 func main() {
-	http.HandleFunc("/google/login", controller.GoogleLogin)
-	http.HandleFunc("/google/callback", controller.GoogleCallback)
-	err := models.ConnectDB()
-	if err != nil {
-		panic(err)
-	}
-	http.ListenAndServe(":3000", nil)
+	models.ConnectDB()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/google/login", controller.GoogleLogin)
+	router.HandleFunc("/google/callback", controller.GoogleCallback)
+
+	router.HandleFunc("/skills", controller.Skills).Methods("GET")
+	router.HandleFunc("/skill", controller.FindSkill).Methods("GET")
+	router.HandleFunc("/skill/{name_map}", controller.DeleteSkill).Methods("DELETE")
+	router.HandleFunc("/skills", controller.CreateSkill).Methods("POST")
+
+	http.ListenAndServe(":3000", router)
 }
