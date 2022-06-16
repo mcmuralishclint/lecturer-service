b1503d579e175b16abe0c19a5bc30bc70dec29fc
muralishc <muralish.clinton@circles.asia>
2022-06-16 22:25:09 +0530

    separate skills router from the ones that need not be protected by the middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/controller"
+	"github.com/mcmuralishclint/personal_tutor/lecturer-service/middleware"
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/models"
 )
 
@@ -15,10 +16,13 @@ func main() {
 	router.HandleFunc("/google/login", controller.GoogleLogin)
 	router.HandleFunc("/google/callback", controller.GoogleCallback)
 
+	skillsRouter := router.PathPrefix("/skills").Subrouter()
+	skillsRouter.Use(middleware.IsAuthorized)
+	skillsRouter.HandleFunc("/skill/{name_map}", controller.DeleteSkill).Methods("DELETE")
+	skillsRouter.HandleFunc("", controller.CreateSkill).Methods("POST")
+
 	router.HandleFunc("/skills", controller.Skills).Methods("GET")
 	router.HandleFunc("/skill", controller.FindSkill).Methods("GET")
-	router.HandleFunc("/skill/{name_map}", controller.DeleteSkill).Methods("DELETE")
-	router.HandleFunc("/skills", controller.CreateSkill).Methods("POST")
 
 	http.ListenAndServe(":3000", router)
 }
