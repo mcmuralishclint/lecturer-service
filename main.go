5770120b78da7f12d32f8248a5fa2f23ada76a52
muralishc <muralish.clinton@circles.asia>
2022-06-16 16:58:26 +0530

    error handling for db connection

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/mcmuralishclint/personal_tutor/lecturer-service/controller"
+	"github.com/mcmuralishclint/personal_tutor/lecturer-service/models"
 )
 
 func main() {
 	http.HandleFunc("/google/login", controller.GoogleLogin)
 	http.HandleFunc("/google/callback", controller.GoogleCallback)
+	err := models.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
 	http.ListenAndServe(":3000", nil)
 }
