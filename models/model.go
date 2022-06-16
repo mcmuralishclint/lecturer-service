422f98a828a32a5f80c8d77243df8a386e113c97
muralishc <muralish.clinton@circles.asia>
2022-06-16 17:24:47 +0530

    fix leak

--- models/model.go
+++ models/model.go
@@ -3,9 +3,12 @@ package models
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 	"reflect"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -26,7 +29,11 @@ type Lecturer struct {
 }
 
 func ConnectDB() error {
-	Client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://mcmuralishclint:mc159357555@my-personal-professor-v.k20xc.mongodb.net/?retryWrites=true&w=majority"))
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	Client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://"+os.Getenv("MONGO_USERNAME")+":"+os.Getenv("MONGO_PASSWORD")+"@my-personal-professor-v.k20xc.mongodb.net/?retryWrites=true&w=majority"))
 	if err != nil {
 		return err
 	}
