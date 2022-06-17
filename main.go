7e4154a4620ac8dd48d94ab183cd7be419655112
muralishc <muralish.clinton@circles.asia>
2022-06-17 19:26:52 +0530

    lecturer skills router

--- main.go
+++ main.go
@@ -13,16 +13,25 @@ func main() {
 	models.ConnectDB()
 	router := mux.NewRouter()
 
+	// Google Login
 	router.HandleFunc("/google/login", controller.GoogleLogin)
 	router.HandleFunc("/google/callback", controller.GoogleCallback)
 
+	// SKills
 	skillsRouter := router.PathPrefix("/skills").Subrouter()
-	skillsRouter.Use(middleware.IsAuthorized)
+	skillsRouter.Use(middleware.IsAdmin)
 	skillsRouter.HandleFunc("/skill/{name_map}", controller.DeleteSkill).Methods("DELETE")
 	skillsRouter.HandleFunc("", controller.CreateSkill).Methods("POST")
 
 	router.HandleFunc("/skills", controller.Skills).Methods("GET")
 	router.HandleFunc("/skill", controller.FindSkill).Methods("GET")
+
+	// Lecturer-Skills
+	lecturerSkillsRouter := router.PathPrefix("/lecturer_skills").Subrouter()
+	lecturerSkillsRouter.Use(middleware.IsAuthorized)
+	lecturerSkillsRouter.HandleFunc("", controller.AllLecturerSkills).Methods("GET")
+	lecturerSkillsRouter.HandleFunc("/", controller.AddLecturerSkills).Methods("POST")
+	// lecturerSkillsRouter.HandleFunc("/", controller.DeleteLecturerSkills).Methods("DELETE")
 
 	http.ListenAndServe(":3000", router)
 }
